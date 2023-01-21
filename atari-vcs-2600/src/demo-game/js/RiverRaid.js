cf5dbdd5ed3fe4c10231a5de8f176f6e5008ea42
aragonxpd154 <marcos.silvabl@live.com>
2023-01-21 10:18:46 -0300

    Testando desenho canvas js
    Realizando alguns teste de canvas js no navegador

---
+++ atari-vcs-2600/src/demo-game/js/RiverRaid.js
@@ -0,0 +1,7 @@
+//Recuperando referência dos objetos no documento
+var canvas = document.getElementById('tela');
+var ctx = canvas.getContext('2d');
+
+// Um pequeno teste (remover depois de testar)
+ctx.fillStyle = '#FF0000'; // Usar cor vemelha
+ctx.fillRect(20, 30, 50, 100); // x=20, y=30, w=50 e h=100
