68c00597ca167fd3383e7f7dc5be36efe817993d
aragonxpd154 <marcos.silvabl@live.com>
2023-01-21 15:54:09 -0300

    Adicionando funções e manipulando canvas js
    Iniciando o construtor e adiconando canvas js

--- atari-vcs-2600/src/demo-game/js/Cenario.js
+++ atari-vcs-2600/src/demo-game/js/Cenario.js
@@ -1,5 +1,5 @@
 function Cenario(riverRaid) {
-  'use strict';
+  ('use strict');
   this.listaMapas = [];
   this.listaMapas.push(mapa01);
   this.mapaAtual = 0;
@@ -13,24 +13,62 @@ function Cenario(riverRaid) {
     Math.floor(riverRaid.canvas.width / Cenario.largura) * Cenario.largura;
   this.linhasPorTela = Math.floor(riverRaid.canvas.height / Cenario.largura);
   this.totalLinhasMatriz = mapa01.length;
+
+  Cenario.todosMapas = [mapa01];
+  Cenario.agua = 0; //Preto
+  Cenario.terra = 1; //Branco
+  Cenario.ponte = 2; //Vermelho
+  Cenario.inimigoParado = 3; //Verde
+  Cenario.inimigoMovel = 4; //Azul
+  Cenario.posto = 5; //Cinza
+
+  this.desenhar = function (ctx) {
+    for (var ym = 0; ym < this.linhasPorTela; ym++) {
+      for (var xm = 0; xm < this.colunasPorTela; xm++) {
+        switch (this.getPosicaoMapa(xm, ym)) {
+          case Cenario.terra:
+            ctx.fillStyle = '#33AA33';
+            ctx.fillRect(
+              this.matrizParaPixelX(xm),
+              this.matrizParaPixelY(ym, true),
+              Cenario.largura,
+              Cenario.largura
+            );
+            break;
+        }
+      }
+    }
+  };
+
+  this.matrizParaPixelX = function (mx) {
+    return Math.floor(mx * Cenario.largura);
+  };
+
+  this.matrizParaPixelX = function (mx) {
+    return Math.floor(mx * Cenario.largura);
+  };
+  this.matrizParaPixelY = function (my, calcDif) {
+    var dif = 0;
+    if (calcDif) {
+      dif = this.posicao - Math.floor(this.posicao);
+    }
+    return (
+      (this.linhasPorTela - 1 - my) * Cenario.largura + dif * Cenario.largura
+    );
+  };
+
+  this.getPosicaoMapa = function (x, y) {
+    var mapa = Math.floor((this.posicao + y) / this.totalLinhasMatriz);
+    var sobra = Math.floor(this.posicao + y) % this.totalLinhasMatriz;
+    if (mapa >= this.listaMapas.length) {
+      this.incluirNovoMapa();
+    }
+    return this.listaMapas[mapa][sobra][x];
+  };
+
+  this.incluirNovoMapa = function () {
+    var n = Math.floor(Math.random() * Cenario.todosMapas.length);
+    var proximoMapa = Cenario.todosMapas[n];
+    this.listaMapas.push(proximoMapa);
+  };
 }
-
-Cenario.todosMapas = [mapa01];
-
-// Preta
-Cenario.agua = 0;
-
-// Branco
-Cenario.terra = 1;
-
-// Vermelho
-Cenario.ponte = 2;
-
-// Verde
-Cenario.inimigoParado = 3;
-
-// Azul
-Cenario.inimigoMovel = 4;
-
-// Cinza
-Cenario.posto = 5;
--- atari-vcs-2600/src/demo-game/js/RiverRaid.js
+++ atari-vcs-2600/src/demo-game/js/RiverRaid.js
@@ -5,3 +5,22 @@ var ctx = canvas.getContext('2d');
 // Um pequeno teste (remover depois de testar)
 ctx.fillStyle = '#FF0000'; // Usar cor vemelha
 ctx.fillRect(20, 30, 50, 100); // x=20, y=30, w=50 e h=100
+
+function RiverRaid(canvasID) {
+  'use strict';
+  this.canvas = document.getElementById(canvasID);
+  this.ctx = this.canvas.getContext('2d');
+  this.temporizador = null;
+  this.emPausa = true;
+  this.gameOver = false;
+  this.cenario = new Cenario(this);
+  this.desenharTudo = function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.cenario.desenhar(this.ctx);
+  };
+  this.desenharTudo();
+}
+
+// Fim do construtor
+
+RiverRaid.instancia = new RiverRaid('tela');
